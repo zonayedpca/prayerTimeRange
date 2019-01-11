bb2dc6a281cb2ac8966f9b3573daefec7640fcbb
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-11 15:22:34 +0000

    Project Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="current-area">
+          <div className="navbar">
+            <div className="brand">
+              PrayerRange
+            </div>
+            <div className="search">
+              <input className="input search" placeholder="Enter Your Location" />
+            </div>
+          </div>
+          <div className="current-details">
+            <div className="current-salaat">
+              <p>It's time for</p>
+              <h2>Fajr</h2>
+            </div>
+            <div className="time-remaining">
+              <p>Time Remaining</p>
+              <h2>00:00</h2>
+            </div>
+          </div>
+        </div>
+        <div className="current-info">
+          <div className="current location">
+            <p>Location</p>
+            <h4>Kharar, Mohali</h4>
+          </div>
+          <div className="current started">
+            <p>Started</p>
+            <h4>00:00</h4>
+          </div>
+          <div className="current will-end">
+            <p>Will End</p>
+            <h4>00:00</h4>
+          </div>
+        </div>
+        <div className="next-area">
+          <div className="section-title">
+            <h3>Next Prayers</h3>
+          </div>
+          <div className="prayers">
+            <div className="prayer general">
+              <h4>Johar</h4>
+              <p>Will Start At 00:00</p>
+            </div>
+            <div className="prayer general">
+              <h4>Achr</h4>
+              <p>Will Start At 00:00</p>
+            </div>
+            <div className="prayer general done">
+              <h4>Magrib</h4>
+              <p>Finished</p>
+            </div>
+            <div className="prayer general">
+              <h4>Esha</h4>
+              <p>Will Start At 00:00</p>
+            </div>
+            <div className="prayer general">
+              <h4>Johar</h4>
+              <p>Will Start At 00:00</p>
+            </div>
+          </div>
+        </div>
+        <div className="footer-area">
+          <div className="footer hadis">
+            <blockquote>Consider if one of you had a river by his door in which he bathed five times a day. Would any filth remain on him?” They said, “No.” Then the Prophet replied: “Likewise, Allah wipes away sins with the five daily prayers.</blockquote>
+          </div>
+          <div className="footer credit">
+            <p>With <strong>❤</strong> By
+              <a href="#">Zonayed Ahmed</a>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/hadis.js
@@ -0,0 +1,16 @@
+export default [
+    'Guard strictly your (habit of) prayers… If you fear, pray on foot, or riding.',
+    'But after them there followed a posterity who missed Prayers and followed after lusts soon, then, will they face destruction, except those who repent and believe, and work righteousness.',
+    'Verily, prayer restrains (oneself) from shameful and unjust deeds…',
+    'Without a doubt, in the remembrance of Allah do hearts find satisfaction.',
+    'And perform prayer… surely the good deeds remove the evils deeds.',
+    'Consider if one of you had a river by his door in which he bathed five times a day. Would any filth remain on him?” They said, “No.” Then the Prophet replied: “Likewise, Allah wipes away sins with the five daily prayers.',
+    'Seek help in patience and prayer.',
+    'Between a man and unbelief is the nonperformance of the Prayer',
+    'Whoever keeps on performing Prayers (in its due time), it will be a light, a proof, and redemption for him on the Day of Judgment. Otherwise, he will be assembled with Pharaoh, Qarun Haman, and ‘Ubai Ibn Khalaf.',
+    'O Mankind, What has deceived you concerning your Lord, the Most Generous?',
+    'Then, there has succeeded them a generation who have given up prayer and have followed their desires. So they will be thrown into Hell. Except those who repent and believe, and work righteousness.',
+    '( the people in Hell will be asked) What has caused you to enter Hell? They will say: We were not of those who used to pray…',
+    'On the Day of Resurrection the feet of the son of Adam will not move away till he is questioned about five matters: on what he spent his life; in doing what he made his youth pass away; where he acquired his property, and on what he spent it; and what he did regarding what he knew.',
+    'Give thanks to Allah, and whoever gives thanks, it is only for his own soul’s good, and whoever is ungrateful, surely Allah is Free of all needs, Worthy of all praise.'
+]
