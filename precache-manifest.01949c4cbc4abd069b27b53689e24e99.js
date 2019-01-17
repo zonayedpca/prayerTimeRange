a60f8c25c60274c174e9e19f4ff4ea805cc80e35
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-17 21:44:03 +0530

    build added

---
+++ precache-manifest.01949c4cbc4abd069b27b53689e24e99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0605c5822bf6129dd28",
+    "url": "/static/js/main.d0605c58.chunk.js"
+  },
+  {
+    "revision": "ff83609d0d082f5a5b19",
+    "url": "/static/js/1.ff83609d.chunk.js"
+  },
+  {
+    "revision": "d0605c5822bf6129dd28",
+    "url": "/static/css/main.9e8419c8.chunk.css"
+  },
+  {
+    "revision": "63c6ed3ae3cbe67478e7e0f561f3f1ed",
+    "url": "/index.html"
+  }
+];
