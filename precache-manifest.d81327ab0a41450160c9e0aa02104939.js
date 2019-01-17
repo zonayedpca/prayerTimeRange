64d34939aac2d5920f0a37af3a32fb45dfd05c92
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-17 21:47:37 +0530

    homepage changed

---
+++ precache-manifest.d81327ab0a41450160c9e0aa02104939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf77e7746a5b0eebe51",
+    "url": "/prayerTimeRange/static/js/runtime~main.1bf77e77.js"
+  },
+  {
+    "revision": "9c098c35bac91d8be95a",
+    "url": "/prayerTimeRange/static/js/main.9c098c35.chunk.js"
+  },
+  {
+    "revision": "ff83609d0d082f5a5b19",
+    "url": "/prayerTimeRange/static/js/1.ff83609d.chunk.js"
+  },
+  {
+    "revision": "9c098c35bac91d8be95a",
+    "url": "/prayerTimeRange/static/css/main.1fb68280.chunk.css"
+  },
+  {
+    "revision": "f0d6ca31eeb28c94647964ca2cf9a79a",
+    "url": "/prayerTimeRange/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01949c4cbc4abd069b27b53689e24e99.js"
+  "/prayerTimeRange/precache-manifest.d81327ab0a41450160c9e0aa02104939.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/prayerTimeRange/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
